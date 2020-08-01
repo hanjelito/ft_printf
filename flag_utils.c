3fed4e329c48ec44ba5bd456aca1803418d29310
Angel Gonzales <angelgonzales@angel.local>
2020-08-01 15:05:02 +0200

    ajustes

--- flag_utils.c
+++ flag_utils.c
@@ -6,7 +6,7 @@
 /*   By: juan-gon <juan-gon@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 14:32:54 by juan-gon          #+#    #+#             */
-/*   Updated: 2020/07/31 18:53:55 by juan-gon         ###   ########.fr       */
+/*   Updated: 2020/08/01 15:04:37 by juan-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,42 @@ void		ft_while(int space, int zero, t_printf *data, int nb)
 {
 	int flag;
 
+	flag = -1;
+	// printf("\n%d precision\n", data->precision);
+	// printf("%d width\n", data->width);
+	// printf("%d len_str\n", data->len_str);
+	// printf("%d len\n", (int)ft_intlen(nb, 10));
+	// printf("%c zero_space\n", data->zero_space);
+	// printf("%c dot\n", data->dot);
+
+
 	if (nb < 0 && (space > data->precision && data->zero_space == '0'))
 	{
 		write(1, "-", 1);
 	}
 	else
 		flag = 0;
+	
+	// printf("\n%d nb", nb);
+	// printf("\n%d flag\n", flag);
+	
 	while (space > 0 && data->tab != '-')
 	{
-		data->len_str += write(1, (data->zero_space != '0' ||
-			(data->dot == '.' && data->precision >= 0)) ? " " : "0", 1);
+		if (data->zero_space != '0' ||(data->dot == '.' && data->precision >= 0))
+			data->len_str += write(1, " ", 1);
+		else
+			data->len_str += write(1, "0", 1);
 		space--;
 	}
-
-	// (nb < 0 && flag == 0) ? ft_putchar_fd('-', 1) : 0;
+	if (nb < 0 && flag == 0){
+		ft_putchar_fd('-', 1);
+	}
 	while (zero-- > 0)
 		data->len_str += write(1, "0", 1);
 	(data->dot == '.' && nb == 0 && data->precision == 0)
-	
 		? 0 : ft_putnbr_fd(nb < 0  ? nb * (-1) : nb , 1);
 	while (space-- > 0)
 		data->len_str += write(1, " ", 1);
 	data->len_str -= (data->dot == '.' && nb == 0 &&
 		data->precision == 0) ? 1 : 0;
-
 }
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: juan-gon <juan-gon@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 10:51:03 by juan-gon          #+#    #+#             */
-/*   Updated: 2020/07/31 18:41:46 by juan-gon         ###   ########.fr       */
+/*   Updated: 2020/08/01 14:52:47 by juan-gon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,40 +70,40 @@ void		ft_formatletter(t_printf *data)
 
 int			ft_printf(const char *format, ...)
 {
-	t_printf	*data;
-	if (!(data = ft_calloc(sizeof(t_printf), 1)))
-		return (0);
-	data->str = (char *)format;
-	va_start(data->arg, format);
-	ft_formatletter(data);
-	va_end(data->arg);
-	free(data);
-	return (data->len_str);
+	t_printf	data;
+	// if (!(data = ft_calloc(sizeof(t_printf), 1)))
+	// 	return (0);
+	data.str = (char *)format;
+	va_start(data.arg, format);
+	ft_formatletter(&data);
+	va_end(data.arg);
+	// free(data);
+	return (data.len_str);
 }
 
 
-int main(void)
-{
+// int main(void)
+// {
 
-// 	// ft_printf("%-05d\n",-7);
-// 	// ft_printf("%-05d\n",-7);
-// 	//    printf("%-05d\n",-7);
-// 	// ft_printf("%05d\n",7);
-// 	// ft_printf("%5d\n",7);
-	// printf("\n|%d|\n", ft_printf("%07d\n", -54));
-	// printf("\n|%d|\n" ,   printf("%07d\n", -54));
-// 	printf("\n|%d|\n", ft_printf("%32s", NULL));
-// ft_printf("%c", "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%");
+// // 	// ft_printf("%-05d\n",-7);
+// // 	// ft_printf("%-05d\n",-7);
+// // 	//    printf("%-05d\n",-7);
+// // 	// ft_printf("%05d\n",7);
+// // 	// ft_printf("%5d\n",7);
+// 	// printf("\n|%d|\n", ft_printf("%07d\n", -54));
+// 	// printf("\n|%d|\n" ,   printf("%07d\n", -54));
+// // 	printf("\n|%d|\n", ft_printf("%32s", NULL));
+// // ft_printf("%c", "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%");
 
-ft_printf("%07i\n", -54);
-ft_printf("%04i\n", -532);
-ft_printf("%04i\n", -4825);
-ft_printf("%7i\n", -14);
-ft_printf("%-7i\n", -14);
-ft_printf("%10.5i\n", -216);
-ft_printf("%.6d\n", -3);
+// ft_printf("%06i\n", -53000);
+// ft_printf("%07i\n", -54);
+// // ft_printf("%04i\n", -4825);
+// // ft_printf("%7i\n", -14);
+// // ft_printf("%-7i\n", -14);
+// // ft_printf("%10.5i\n", -216);
+// // ft_printf("%.6d\n", -3);
 
-//    printf("%d\n", 11);
-// ft_printf("%-07s\n", "hello");
-//    printf("%-07s\n", "hello");
-}
+// //    printf("%d\n", 11);
+// // ft_printf("%-07s\n", "hello");
+// //    printf("%-07s\n", "hello");
+// }
